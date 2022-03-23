8f26c2720b34329b086c1f3a4611c5d902cdac14
Marcelo Pinto <99257490+pinto-marcelo@users.noreply.github.com>
2022-03-23 14:14:10 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:square_art_hp/square_art_hp.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Square Art Home Page',
+      theme: ThemeData(
+        backgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SquareArtHP(),
+    );
+  }
+}
